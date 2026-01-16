7dd8b9add970110c7b1e155ef9828afe00134e0d
Lovish <lovishk72221@gmail.com>
2026-01-16 21:50:40 +0100

    did advanced component with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import Button from "./components/Button";
-import Input from "./components/Input";
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
 
 function App() {
   return (
-    <main>
-      <p>
-        <Button>A Button</Button>
-        <Button href="https://google.com">A Link</Button>
-      </p>
-    </main>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
 
